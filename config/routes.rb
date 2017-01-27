b1ae014eb20d93bb1cd8d924a1e954b0c6727ab6
Sonia Arai <soninhanaomi@gmail.com>
2017-01-27 15:45:58 -0200

    create routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :products
+  resources :markets
+
+  # only for farmer control
+  get 'overview', to: "overview#index"
 end
