091dae27499d6c1d1f521b89ba45490ae1aebc8e
ana mukoyama <mukoyama.rockstar@gmail.com>
2017-01-27 11:58:06 -0200

    add name to farmers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127134438) do
+ActiveRecord::Schema.define(version: 20170127135204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20170127134438) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "mobile"
+    t.string   "address"
+    t.string   "zipcode"
     t.index ["email"], name: "index_farmers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_farmers_on_reset_password_token", unique: true, using: :btree
   end
