b911c3cf260f29cbd61b9df4c6fc69ae073549b5
ana mukoyama <mukoyama.rockstar@gmail.com>
2017-03-30 15:18:06 -0300

    update schema model Farmer

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,6 +1,7 @@
 class Farmer < ApplicationRecord
   has_many :stalls
   has_many :markets, through: :stalls
+  has_many :orders, through: :stalls
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
