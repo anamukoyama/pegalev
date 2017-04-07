0bdf96843afa857822ec7d5866e118965243d2e3
anamukoyama <mukoyama.rockstar@gmail.com>
2017-04-07 15:00:40 -0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :stall
   has_many :order_items
-  has_many :products, :through => :order_items
+  has_many :products, through: :order_items
 end
