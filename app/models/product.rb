c6291a26fad4a1f7c06540ae5e58e254db5522c6
anamukoyama <mukoyama.rockstar@gmail.com>
2017-04-07 15:01:21 -0300

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   CATEGORIES = %w(Frutas Legumes Verduras)
   mount_uploader :photo, PhotoUploader
   has_many :farmer_products
-  has_many :stalls, :through => :farmer_products
+  has_many :stalls, through: :farmer_products
   has_many :order_items
-  has_many :orders, :through => :order_items
+  has_many :orders, through: :order_items
 end
