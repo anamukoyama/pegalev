f068a9120298270d8360f6d5a3db38e28ca3a1e5
anamukoyama <mukoyama.rockstar@gmail.com>
2017-04-07 15:01:37 -0300

    Update stall.rb

--- app/models/stall.rb
+++ app/models/stall.rb
@@ -3,5 +3,5 @@ class Stall < ApplicationRecord
   belongs_to :market
   has_many :orders
   has_many :farmer_products
-  has_many :products, :through => :farmer_products
+  has_many :products, through: :farmer_products
 end
