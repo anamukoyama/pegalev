b2c36ef965f291ddf483efd9af8be881f625bb3f
Fillype Farias <fillype1@hotmail.com>
2017-04-10 17:32:48 -0300

    paginas no overview e correção nos relacionamentos do model

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,4 +1,17 @@
 class OverviewController < ApplicationController
   def index
   end
+
+  def my_orders
+  #  @my_orders = current_seller.orders
+  end
+
+  def my_products
+   @my_products = current_seller.seller_products
+   @products = Product.all
+  end
+
+  def my_stalls
+  #  @my_stalls = current_seller.markets
+  end
 end
--- app/models/stall.rb
+++ app/models/stall.rb
@@ -2,6 +2,4 @@ class Stall < ApplicationRecord
   belongs_to :seller
   belongs_to :market
   has_many :orders
-  has_many :seller_products
-  has_many :products, through: :seller_products
 end
