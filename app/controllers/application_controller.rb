14ea155043ea9cf8e2e5818073d890e79787227f
Fillype Farias <fillype1@hotmail.com>
2017-04-12 15:42:24 -0300

    delete unused section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
 
-  def current_user=(user)
-    @current_user = user
-  end
 end
