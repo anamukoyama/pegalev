7d27a8ea99856942daef64991ca4599d5d6b2630
Fillype Farias <fillype1@hotmail.com>
2017-05-15 10:02:19 -0300

    add to bascket

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,39 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @sellers = sellers(params["sellers"])
+    @random_seller = @sellers.sample
     @product = Product.find(params[:id])
+    @market  = Market.find(params["market_id"].to_i)
   end
+
+  def add_to_bascket
+    # stall = Stall.where(seller_id: params["seller"], market_id: params["market_id"].to_i).first
+    user_verification
+    # Order.new(user_id: current_user.id, stall_id: stall.id )
+    # flash 'produto adicionado com sucesso!'
+  end
+
 
   private
 
-  def product_params
-    params.require(:product).permit(:name, :price, :category, :best_before, :photo, :photo_cache)
+  # recebe os um array com os id's dos feirantes deste produto especifico
+  # e devolve um array com as suas referencias do banco de dados
+  def sellers(ids)
+    list_sellers = []
+    ids.each do |id|
+      list_sellers << Seller.find(id)
+    end
+    list_sellers
   end
+
+  def user_verification
+    if current_user == nil
+      redirect_to new_user_session_path
+    end
+  end
+
+  # def product_params
+  #  params.require(:product).permit(:name, :price, :category, :best_before, :photo, :photo_cache)
+  # end
 end
