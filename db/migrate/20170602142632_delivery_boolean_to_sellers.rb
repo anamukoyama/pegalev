023b9ffeb52c338b7eb25ba3a9529b8142aa9bdc
Fillype Farias <fillype1@hotmail.com>
2017-06-02 11:46:32 -0300

    substituir boolean por default: false

--- db/migrate/20170602142632_delivery_boolean_to_sellers.rb
+++ db/migrate/20170602142632_delivery_boolean_to_sellers.rb
@@ -1,5 +1,6 @@
 class DeliveryBooleanToSellers < ActiveRecord::Migration[5.0]
   def change
+    remove_column :sellers, :delivery, :boolean
     add_column :sellers, :delivery, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602142409) do
+ActiveRecord::Schema.define(version: 20170602142632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,18 +110,18 @@ ActiveRecord::Schema.define(version: 20170602142409) do
   end
 
   create_table "sellers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
     t.string   "mobile"
     t.string   "address"
@@ -130,7 +130,7 @@ ActiveRecord::Schema.define(version: 20170602142409) do
     t.string   "state"
     t.integer  "number"
     t.string   "complement"
-    t.boolean  "delivery"
+    t.boolean  "delivery",               default: false
     t.index ["email"], name: "index_sellers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true, using: :btree
   end
