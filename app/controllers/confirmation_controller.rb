c8a3e77082d54009db9c1e1a7f6985a53f8adf1f
Fillype Farias <fillype1@hotmail.com>
2017-06-16 16:08:17 -0300

    adicionar itens e preços no choose address

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -2,17 +2,22 @@ class ConfirmationController < ApplicationController
 
   def choose_address
     result = Geocoder.search(session[:cep]).first.data
-    @street = session[:rua]
-    @number = session[:numero]
-    @district = session[:bairro]
-    @city = session[:cidade]
-    # @city = result.first[1][3]["long_name"]     # cidade
+    get_coockies
   end
 
   def payment
   end
 
   def review
+  end
+
+  private
+
+  def get_coockies
+    @street = session[:rua]
+    @number = session[:numero]
+    @district = session[:bairro]
+    @city = session[:cidade]
   end
 
 end
