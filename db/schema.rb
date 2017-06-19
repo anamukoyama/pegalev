0c08845830054253f0b794cbb787ee9cf2648d4d
Fillype Farias <fillype1@hotmail.com>
2017-06-19 11:33:55 -0300

    remove order_statuses_id integer column from orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619142723) do
+ActiveRecord::Schema.define(version: 20170619143237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +64,10 @@ ActiveRecord::Schema.define(version: 20170619142723) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.integer  "stall_id"
-    t.integer  "order_statuses_id"
     t.decimal  "subtotal"
     t.decimal  "total"
     t.decimal  "shipping"
