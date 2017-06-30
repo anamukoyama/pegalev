c0385a7ebe6b456ac5e9ac90f7e3eb2381cf6466
ana mukoyama <mukoyama.rockstar@gmail.com>
2017-06-30 14:54:17 -0300

    fix migration

--- db/migrate/20170602142632_delivery_boolean_to_sellers.rb
+++ db/migrate/20170602142632_delivery_boolean_to_sellers.rb
@@ -1,6 +1,5 @@
 class DeliveryBooleanToSellers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :sellers, :delivery, :boolean
     add_column :sellers, :delivery, :boolean, default: false
   end
 end
